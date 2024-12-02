244a189d24ee00a843ceefb69b3987a358489dff
awsandgit <aws4em@gmail.com>
2024-12-02 11:10:01 +0530

    Daily backup Monday 02 December 2024 11:10:01 AM IST.

--- config.el
+++ config.el
@@ -94,3 +94,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(setq httpd-port 8085)
+(use-package simple-httpd
+  :ensure t)
--- packages.el
+++ packages.el
@@ -11,6 +11,7 @@
 (package! lsp-python-ms)
 (package! super-save)
 (package! catppuccin-theme)
+(package! simple-httpd)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
